1453aa6ed2ed73f6cc1ac18c9ab32f6398e74387
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-12-23 00:32:09 +0530

    Boiler Plate for matter.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world');
+const { Engine, World, Render, Bodies, Runner } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600
+  }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// Walls
